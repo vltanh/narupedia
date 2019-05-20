40c6d6dec0d54d40da90fb61a553d11539d3b4ef
vltanh <1612838@student.hcmus.edu.vn>
2019-05-20 14:55:53 +0700

    remove unnecessary files

---
+++ src/components/Users.vue
@@ -0,0 +1,52 @@
+<template>
+  <div style="margin-top: 0px;">
+    <div v-for="(user, id) in users" v-bind:key="id">
+      <div
+        v-bind:class="[activeUser == user.id ? 'user active' : 'user']"
+        v-on:click="chat(user.id)"
+      >
+        {{user.userName}}
+        <span v-if="user.has_new_message" class="has_new_message">New message</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Users",
+  props: {
+    users: Array
+  },
+  data() {
+    return {
+      activeUser: null
+    };
+  },
+  methods: {
+    chat: function(id) {
+      this.activeUser = id;
+      this.$emit("chat", id);
+    }
+  }
+};
+</script>
+<style scoped>
+.user {
+  margin: 0px !important;
+  padding: 10px 4px 10px 8px;
+}
+.active {
+  background: #3498db;
+  color: white;
+}
+.has_new_message {
+  background-color: #3498db;
+  display: inline-block;
+  color: white;
+  margin-bottom: -4px;
+  font-size: 10px;
+  margin: 4px;
+  padding: 3px;
+  font-weight: bolder;
+}
+</style>
